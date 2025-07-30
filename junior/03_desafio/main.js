2d8bc25f64f992dbcaac10e67502a931c07ad240
reotsugua <renanaughusto.ras@gmail.com>
2025-07-30 17:03:05 -0300

    desafio3

---
+++ junior/03_desafio/main.js
@@ -0,0 +1,48 @@
+const valoresRepetidos = [10, 2, 3, 4, 5, 6, 6, 8, 9, 10, 10, 11, 10, 12, 13, 13, 99, 99, 99]
+
+function removerDuplicatasComSet(array) {
+    const semRepeticao = new Set(array);
+    return [...semRepeticao];
+}
+
+function removerDuplicatasComForESplice(array) {
+    const novoArray = array;    
+    
+    array.forEach(element => {
+        
+        var contador = 0;
+        for (let index = 0; index <= novoArray.length-1; index++) {
+            const elemento = novoArray[index];
+            
+            if (element === elemento) {
+                contador++
+                
+                if (contador > 1) {     
+                    novoArray.splice(index, 1) 
+                }
+            }
+        }
+        contador = 0;
+    });
+
+    return novoArray;
+}
+
+
+function removerDuplicatasComFilter(array) {
+    let novoArray = array;    
+    
+    array.forEach(element => {
+        
+        novoArray = novoArray.filter(n => n !== element);
+        novoArray.push(element);
+
+    });
+
+    return novoArray;
+}
+
+
+
+
+ 
---
+++ junior/03_desafio/readme.md
@@ -0,0 +1,29 @@
+🟡 Desafio 3: Remover Duplicatas de um Array
+Crie uma função chamada removerDuplicatas que:
+
+Recebe um array de valores (números ou strings).
+
+Retorna um novo array sem valores repetidos, mantendo a ordem original.
+
+🎯 Exemplo:
+js
+Copiar
+Editar
+removerDuplicatas([1, 2, 2, 3, 4, 4, 5]) 
+// → [1, 2, 3, 4, 5]
+
+removerDuplicatas(["a", "b", "a", "c", "b"]) 
+// → ["a", "b", "c"]
+📚 Requisitos:
+Pode usar Set, filter, includes, indexOf, etc.
+
+Não usar bibliotecas externas (tipo lodash).
+
+💡 Dica:
+Esse desafio testa:
+
+Manipulação de arrays.
+
+Conhecimento de estruturas de dados (Set, map, etc).
+
+Capacidade de manter ordem original ao remover duplicatas.
