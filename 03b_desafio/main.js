f2360256baa3f26db292f1ab82e80a5def8a069d
reotsugua <renanaugusto.ras@gmail.com>
2025-08-01 15:08:12 -0300

    desafio 3b peimria parte

--- 03b_desafio/main.js
+++ 03b_desafio/main.js
@@ -1,4 +1,38 @@
-const valoresRepetidos = [10, 2, 3, 4, 5, 6, 6, 8, 9, 10, 10, 11, 10, 12, 13, 13, 99, 99, 99]
+const valoresRepetidos = [10, 2, 3, 4, 5, 6, 6, 8, 9, 10, 10, 11, 10, 12, 13, 13, 99, 99, 99];
+const nomes = ["Lucas", "Ana", "Carlos", "Carlos", "Carlos", "Mariana", "João", "Beatriz" , "Beatriz"];
 
+
+
+
+
+ function contarFrequenciaComReduce(array){
+    const frequencia = {}
+
+    
+
+    return frequencia;
+ }
+
+ function contarFrequenciaNaMao(array){
+    const frequencia = {}
+
+    for (const element of array) {
+        frequencia[element] ? frequencia[element] = parseInt(frequencia[element]) +1 : frequencia[element] = 1;
+    }
+
+    return frequencia;
+ }
+
+ function contarFrequenciaForEachSemIf(array){
+    const frequencia = {}
+
+    array.forEach(element => {
+        frequencia[element] = (frequencia[element] || 0) + 1;
+    });
+
+    return frequencia;
+ }
+
+console.log(contarFrequenciaComReduce(nomes));
 
  
